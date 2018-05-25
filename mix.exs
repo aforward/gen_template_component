7d7711bcc71358d084b4b5930a5de22314afe3e5
Andrew Forward <aforward@gmail.com>
2018-05-25 10:20:07 -0400

    Add mix format compatibility

--- mix.exs
+++ mix.exs
@@ -1,48 +1,48 @@
 defmodule GenTemplateComponent.Mixfile do
   use Mix.Project
 
-  @name    :gen_template_component
+  @name :gen_template_component
   @version "0.1.0"
 
   @deps [
-    { :mix_templates,  ">0.0.0",  app: false },
-    { :ex_doc,         ">0.0.0",  only: [:dev, :test] },
+    {:mix_templates, ">0.0.0", app: false},
+    {:ex_doc, ">0.0.0", only: [:dev, :test]}
   ]
 
   @maintainers ["Your Name <youe@example.com>"]
-  @github      "https://github.com/•••/#{@name}"
+  @github "https://github.com/•••/#{@name}"
 
   @description """
   What your template does (less than 300 characters)
   """
-  
+
   # ------------------------------------------------------------
-  
+
   def project do
-    in_production = Mix.env == :prod
+    in_production = Mix.env() == :prod
+
     [
-      app:     @name,
+      app: @name,
       version: @version,
-      deps:    @deps,
-      elixir:  "~> 1.4",
+      deps: @deps,
+      elixir: "~> 1.4",
       package: package(),
-      description:     @description,
-      build_embedded:  in_production,
-      start_permanent: in_production,
+      description: @description,
+      build_embedded: in_production,
+      start_permanent: in_production
     ]
   end
 
   defp package do
     [
-      name:        @name,
-      files:       ["lib", "mix.exs", "README.md", "LICENSE.md", "template"],
+      name: @name,
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md", "template"],
       maintainers: @maintainers,
-      licenses:    ["Apache 2.0"],
-      links:       %{
-        "GitHub" => @github,
-      },
-#      extra:       %{ "type" => "a_template_for_mix_gen" },
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github
+      }
+      #      extra:       %{ "type" => "a_template_for_mix_gen" },
     ]
   end
-  
 end
