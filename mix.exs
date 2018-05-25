907193b377b5b152951549b98c5f5ff4f961317b
Andrew Forward <aforward@gmail.com>
2018-05-25 11:30:24 -0400

    v0.1.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule GenTemplateComponent.Mixfile do
   use Mix.Project
 
   @name :gen_template_component
-  @version "0.1.1"
+  @version "0.1.2"
 
   @deps [
     {:mix_templates, ">0.0.0", app: false},
